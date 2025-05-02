105a3a08e85abc740ca35130455fae0a9c04beb5
Noah Asano <pili.asano@gmail.com>
2025-05-02 08:34:32 -1000

    Make working tasks and projects
    This commit will make tasks and projects that work. Meaning, when you submit a form for a new project, it is added to the display and when you click on the project, the tasks for that project will show. When you create a new task, it adds to the project you specify. Further work needs to be done to make tasks display dynamically update when you submit the new task form. Task and project elements also need functionality so you can edit and delete them.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,50 @@
 //imports
 import './styles.css';
 import './forms.css';
-import {createTask, createProjectArray, lightDarkButton, renderProjects} from "./toDo.js"
+import {createTask, createProjectArray, lightDarkButton, renderProjects, addTaskToProject, updateProjectSelect, renderTasks} from "./toDo.js"
 
 //array of projects
 const projects = []
 
 //js for task items
-const newTaskButton = document.getElementById("new-task-button")
-const newTaskDialog = document.querySelector("#taskDialog")
-const newTaskForm = document.querySelector("#taskForm")
+    const newTaskButton = document.getElementById("new-task-button")
+    const newTaskDialog = document.querySelector("#taskDialog")
+    const newTaskForm = document.querySelector("#taskForm")
 
-newTaskButton.addEventListener("click", () => {newTaskDialog.showModal()})
-lightDarkButton(newTaskButton)
+    //when you click to create a new task, the form shows and options dynamically update
+    newTaskButton.addEventListener("click", () => {
+        newTaskDialog.showModal()
+        updateProjectSelect(projects)
+    })
+    lightDarkButton(newTaskButton)
 
-//left off here//
+    //when you submit the new task form, the task is added to the proper project
+    newTaskForm.addEventListener("submit", (e) => {
+        e.preventDefault()
+        const newTask = createTask(newTaskForm)
+        addTaskToProject(newTask, projects)
+        newTaskForm.reset()
+        newTaskDialog.close()
+    })
 
 //js for project items
-const newProjectButton = document.getElementById("new-project-button")
-const newProjectDialog = document.querySelector("#projectDialog")
-const newProjectForm = document.querySelector("#projectForm")
+    const newProjectButton = document.getElementById("new-project-button")
+    const newProjectDialog = document.querySelector("#projectDialog")
+    const newProjectForm = document.querySelector("#projectForm")
 
-newProjectButton.addEventListener("click", () => {newProjectDialog.showModal()})
-lightDarkButton(newProjectButton)
+    //when you click to create a new task, the form shows
+    newProjectButton.addEventListener("click", () => {newProjectDialog.showModal()})
+    lightDarkButton(newProjectButton)
 
-newProjectForm.addEventListener("submit", (e) => {
-    projects.push(createProjectArray(newProjectForm));
-    e.preventDefault();
-    newProjectForm.reset();
-    newProjectDialog.close();
-    renderProjects(sidebar, projects, taskViewport)
-})
+    /*when you submit the new project form, the project it added to your array of projects and rendered on the
+    screen. In the render function, event listeners are added so that the tasks render on click */
+    newProjectForm.addEventListener("submit", (e) => {
+        e.preventDefault()
+        projects.push(createProjectArray(newProjectForm))
+        newProjectForm.reset()
+        newProjectDialog.close()
+        renderProjects(sidebar, projects, taskViewport)
+    })
 
 //sidebar js
 const sidebar = document.querySelector(".projects")
--- src/toDo.js
+++ src/toDo.js
@@ -1,53 +1,84 @@
-export function createTask(form) {
-    const title = form.querySelector("#title").value;
-    const description = form.querySelector("#description").value;
-    const dueDate = form.querySelector("#dueDate").value;
-    const priority = form.querySelector("#priority").value;
-    return {title, description, dueDate, priority}
-}
+//code for project and task objects
+    export function createTask(form) {
+        const title = form.querySelector("#title").value;
+        const description = form.querySelector("#description").value;
+        const dueDate = form.querySelector("#dueDate").value;
+        const priority = form.querySelector("#priority").value;
+        const parentProject = form.querySelector("#parentProject").value;
 
-export function createProjectArray(form) {
-    let projectArray = []
-    const title = form.querySelector("#projectTitle").value;
-
-    function addToProject(item) {
-        projectArray.push(item);
+        return {title, description, dueDate, priority, parentProject}
     }
 
-    function removeFromProject(item) {
-        projectArray = projectArray.filter(element => element !== item);
+    export function addTaskToProject(task, projectArray) {
+        projectArray.forEach((project) => {
+            if (task.parentProject == project.title) {
+                project.projectArray.push(task)
+            }
+        })
     }
 
-    return {title, projectArray, addToProject, removeFromProject}
-}
+    export function createProjectArray(form) {
+        let projectArray = []
+        const title = form.querySelector("#projectTitle").value;
 
-export function lightDarkButton(button) {
-    button.addEventListener("mouseenter", () => {
-        button.style.filter = "brightness(80%)"})
-      button.addEventListener("mouseleave", () => {
-        button.style.filter = "brightness(100%)"})    
-}
+        return {title, projectArray}
+    }
 
-//left off here
-export function renderTasks(taskViewport, project) {
-    taskViewport.innerHTML = "<p>Your Tasks</p>"
-    project.projectArray.forEach((task) => {
-        const taskElement = document.createElement("div")
-        taskElement.innerHTML = `Title: ${task.title} Description: ${task.description} 
-        Due: ${task.dueDate} Priority: ${task.priority}`
-        taskElement.classList.add("task-item")
-        taskViewport.appendChild(taskElement)
-    })
-}
+//user interface functionality (stuff that's not necessary but looks nice)
+    export function lightDarkButton(button) {
+        button.addEventListener("mouseenter", () => {
+            button.style.filter = "brightness(80%)"})
+        button.addEventListener("mouseleave", () => {
+            button.style.filter = "brightness(100%)"})    
+    }
 
-export function renderProjects(sidebar, projects, taskViewport) {
-    sidebar.innerHTML = "<p>Your Projects</p>"
+    function updateTitle(title) {
+        const projectTitle = document.querySelector(".tasks-title")
+        projectTitle.innerHTML 
+        = `<p>Your Tasks > ${title} <p>`
+        }
 
-    projects.forEach(project => {
-        const projectElement = document.createElement("p")
-        projectElement.textContent = project.title
-        projectElement.classList.add("project-item")
-        projectElement.addEventListener("click", () => renderTasks(taskViewport, project))
-        sidebar.appendChild(projectElement)
-    });
-}
+//real functionality
+    export function updateProjectSelect(projects) {
+        const projectSelect = document.querySelector("#parentProject")
+        projectSelect.innerHTML = " "
+
+        projects.forEach((project) => {
+            const option = document.createElement("option")
+            option.value = project.title
+            option.textContent = project.title
+            projectSelect.appendChild(option)
+        })
+    }
+
+    export function renderTasks(taskViewport, project) {
+        taskViewport.innerHTML = ""
+        project.projectArray.forEach((task) => {
+            const newTaskElement = document.createElement("div")
+            newTaskElement.classList.add("task")
+
+            newTaskElement.innerHTML = `${task.title}`
+
+            //add edit, delete event listeners, display
+
+            taskViewport.appendChild(newTaskElement)
+        })
+    }
+
+    export function renderProjects(sidebar, projects, taskViewport) {
+        sidebar.innerHTML = "<p>Your Projects</p>"
+
+        projects.forEach(project => {
+            const projectElement = document.createElement("p")
+            projectElement.textContent = project.title
+            projectElement.classList.add("project-item")
+
+            projectElement.addEventListener("click", () => {
+                renderTasks(taskViewport, project)
+                updateTitle(project.title)
+            })
+
+            projectElement.style.borderBottom = ("2px solid black")
+            sidebar.appendChild(projectElement)
+        });
+    }
